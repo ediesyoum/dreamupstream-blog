e43534a4b01b3b94b0237d06c07f7f8c8ad39de5
Eden Syoum <eden@temple.edu>
2016-07-10 16:29:21 -0400

    Commit 18: Displaying Comments in each Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,15 @@ class Post
 
   @@all = []
 
-  attr_accessor :title
+  attr_accessor :title, :comments
 
   def initialize(title)
     @title = title
+    @comments = [
+      "Comment A",
+      "Comment B",
+      "Comment C"
+    ]
     @@all.push(self)
   end
 
